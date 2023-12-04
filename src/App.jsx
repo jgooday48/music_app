6e03d0e3ef6fddb9c79ae3f81359741f5f90af8f
jgooday48 <joelgooday48@gmail.com>
2023-12-04 12:18:51 +0000

    allow user to increment like by 1 only

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const oasisAlbums = [
+const oasisAlbums = [ // object with all Oasis albums
   {title: 'Definitely Maybe', year:1994, id:1},
   {title:'Whats the Story Morning Glory', year:1995, id:2},
   {title:'Be Here Now', year:1997, id:3},
@@ -17,15 +17,15 @@ const oasisAlbums = [
 ]
 
 
-const albumList = oasisAlbums.map(album => 
+const albumList = oasisAlbums.map(album =>  // lists all albums and their release years
   <li key={album.id}>
 
   {album.title}: {album.year}
   </li>)
 
-const intro = (
+const intro = ( // Intro statement
   <div>
-  <p>Bursting onto the britpop scene in 1994, Oasis were a force to be reckoned with during the mid-late 90's. Their genre is britpop/rock</p>
+  <p>Bursting onto the britpop scene in 1994, Oasis were a force to be reckoned with during the mid-late 90's. Their genre is britpop/rock.</p>
   <p>The band consided of the famous Gallagher brothers: Liam Gallaher and Noel Gallagher</p>
   <p>Their sophomore album What's the Story Morning Glory catapulted the band to superstar status. It is considered one of the greatest albums ever made and remains one of the UKs 
     greatest selling albums.
@@ -39,25 +39,27 @@ const intro = (
 
 function App() {
   
-  const [counter, setCounter] = useState(0)
-  
+  const [counter, setCounter] = useState(0); // set hook
+
   const likeBtn = () => {
-    setCounter(previousState => previousState+1)
-  }
-  const dislikeBtn = () => {
-    setCounter(previousState => previousState-1)
-  }
+
+    setCounter((previousState) => (previousState === 0 ? 1 : previousState - 1)); // ternary statement allows user to add 1 like and retract if they wish
+  };
+  
+
   return (
     <>
-    <h1>Oasis' greatest albums</h1>
+    <h1>Oasis Portfolio</h1>
     {/* <br /> */}
     <p>{intro}</p>
     <br />
     <h2>Album List</h2>
-    <ul>{albumList}</ul>
-    <h3>Like: { counter }</h3>
-    <button onClick={likeBtn}>Like</button>
-    <button onClick={dislikeBtn}>Dislike</button>
+    <ul>{albumList}
+    <button onClick={likeBtn}> { counter } Likes</button>
+    
+    </ul>
+
+ 
 
     </>
   )
