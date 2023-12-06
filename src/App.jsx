02d08777061251461ed63921b95524d50cd5b654
jgooday48 <joelgooday48@gmail.com>
2023-12-06 12:19:43 +0000

    put front page in pages/Home

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import './App.css'
 
 
 function App() {
-  const [likeCounters, setLikeCounters] = useState([0]) // initialise a new array, fills with 0s for each album
-  const [inputText, setInputText] = useState('')
-  const [comments, setComments] = useState([]) // initialise empty array for comments
-
-
 
 
   return (
@@ -30,16 +25,6 @@ function App() {
 
 
 </Routes>
-      <h1>Oasis Portfolio</h1>
-      <IntroText />
-      
-      <h2>Album List</h2>
-      < Albums likeCounters={likeCounters} setLikeCounters={setLikeCounters}/>
-
-      <h3>Comments</h3>
-
-
-      <Submit inputText={inputText} setInputText={setInputText} comments={comments} setComments= {setComments}/>
 
     </div>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import * as components from '../../components'
 const Home = () => {
+  const [likeCounters, setLikeCounters] = useState([0]) // initialise a new array, fills with 0s for each album
+  const [inputText, setInputText] = useState('')
+  const [comments, setComments] = useState([]) // initialise empty array for comments
   return (
-    <div>Home Page</div>
+    <div>
+
+      <h1>Oasis Portfolio</h1>
+      <components.IntroText />
+      
+      <h2>Album List</h2>
+      < components.Albums likeCounters={likeCounters} setLikeCounters={setLikeCounters}/>
+
+      <h3>Comments</h3>
+
+
+      <components.Submit inputText={inputText} setInputText={setInputText} comments={comments} setComments= {setComments}/>
+    </div>
   )
 }
 
